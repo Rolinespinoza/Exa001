909f16d06c145b5630d010c456976e87abec14e6
UPEU <UPEU@172.17.1.22>
2017-04-25 20:40:18 -0500

    v1: version 1 del examen 01-reeditada

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -5,3 +5,8 @@
  */
 
 
+var btnMenu = document.getElementById("btnmenu");
+var nav = document.getElementById("nav");
+
+btnMenu.addEventListener("click", function(){
+	nav.classList.toggle("mostrar");})
